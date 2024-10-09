cfa54d17ad83412dbaf6f21afe6c6bff039aeb7a
pavi <paviguna116@gmail.com>
2024-10-10 02:08:29 +0530

    Committing changes in chat-app submodule

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'; 
+import Chat from './pages/Chat/Chat';
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import SetAvatar from "./pages/SetAvatar/SetAvatar";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/setAvatar" element={<SetAvatar/>} />
+        <Route path="/" element={<Chat/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
